2afa07ee97a85a410f529c87c1166745b49c47b7
Devansh Gaur <100227472+devansh1012007@users.noreply.github.com>
2026-02-07 13:47:35 +0530

    just name rn

--- app_1/serializers.py
+++ app_1/serializers.py
@@ -125,13 +125,13 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'password', 'email')
+        fields = ('first_name', 'password', 'email')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = User.objects.create_user(
             first_name=validated_data['first_name'],
-            last_name=validated_data['last_name'],
+            #last_name=validated_data['last_name'],
             email=validated_data['email'],
             password=validated_data['password']
         )
